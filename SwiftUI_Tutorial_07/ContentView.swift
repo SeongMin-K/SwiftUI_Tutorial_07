dd7dfd52e6e64f33f631ce51fa3e0a9165d00296
SeongMin-K <eo1tks3@naver.com>
2021-12-21 13:20:20 +0900

    Initial Commit

---
+++ SwiftUI_Tutorial_07/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Tutorial_07
+//
+//  Created by SeongMinK on 2021/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Tutorial_07/SwiftUI_Tutorial_07App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tutorial_07App.swift
+//  SwiftUI_Tutorial_07
+//
+//  Created by SeongMinK on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tutorial_07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
